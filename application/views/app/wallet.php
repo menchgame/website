4baf80b40e173da5494a899ea82d3083ecaa3517
shervinesta <shervin@mench.com>
2025-01-10 13:24:12 -0500

    Simple updates

--- application/views/app/wallet.php
+++ application/views/app/wallet.php
@@ -25,7 +25,7 @@ echo '<div class="center miscstats hideIfEmpty"></div>';
 
 foreach($this->config->item('e___33292') as $e__id1 => $m1) {
 
-    echo '<h3 class="center centerh main__title" title="@'.$m1['m__handle'].'"><div class="large-cover">'.$m1['m__cover'].'</div><b class="card_count_'.$e__id1.'"><i class="fas fa-yin-yang fa-spin"></i></b> '.$m1['m__title'].':</h3>';
+    echo '<h3 class="center centerh main__title" title="@'.$m1['m__handle'].'"><div class="large-cover hidden">'.$m1['m__cover'].'</div><b class="card_count_'.$e__id1.'"><i class="fas fa-yin-yang fa-spin"></i></b> '.$m1['m__title'].':</h3>';
 
     echo '<div class="row justify-content list-covers">';
     
@@ -35,7 +35,7 @@ foreach($this->config->item('e___33292') as $e__id1 => $m1) {
         echo '<div class="card_frame dropdown_d'.$e__id1.' dropdown_'.$e__id2.'" e__id="'.$e__id2.'">';
 
         echo '<div title="'.$m2['m__message'].'">';
-        //echo '<div class="large_cover">'.$m2['m__cover'].'</div>';
+        echo '<div class="large_cover">'.$m2['m__cover'].'</div>';
         echo '<div class="main__title large_title"><b class="card_count_'.$e__id2.'"><i class="fas fa-yin-yang fa-spin"></i></b></div>';
         echo '<div class="main__title large_title" title="@'.$m2['m__handle'].'">'.$m2['m__title'].'</div>';
         echo '</div>';
